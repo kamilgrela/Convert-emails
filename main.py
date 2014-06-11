a9fa33aa8fc2cbba320916c77fa606a6ac1c9d05
Kamil Grela <kamil.grela@stxnext.com>
2014-06-11 17:08:22 +0200

    [1] Changed thinking :)

--- main.py
+++ main.py
@@ -1,73 +1,70 @@
 # -*- coding: utf-8 -*-
 import io
-dictionary = {}
+import re
 maps = [u'ę', u'ó', u'ą', u'ś', u'ł', u'ż', u'ź', u'ć', u'ń']
-
-
-def transform(word):
-    encoded_word = u""
-    for c in word:
-        if c in maps:
-            encoded_word = encoded_word + u'ďż˝'
-        else:
-            encoded_word = encoded_word + c
-    return encoded_word
-
-# TODO: zdania
-# def enter_text():
-#     string = u""
-#     text = raw_input('Enter text: ')
-#     text = text.decode('utf-8')
-#     text = text.split()
-#     print text
-#     for place, word in enumerate(text):
-#         if u'ďż˝' in word:
-#             good_word = answer(word)
-#             text[place] = good_word
-#         else:
-#             pass
-#     string = u' '.join(str(e) for e in text)
-#     print string
-
-# pojedyncze slowo
-def question():
-
-    while True:
-        malformed = raw_input('Enter malformed word: ')
-        malformed = malformed.decode('utf-8')
-        answer(malformed)
-
-
-def answer(malformed):
-    if malformed in dictionary:
-        for word in dictionary[malformed]:
-            print word
-    else:
-        print "{} dosen't match any of words.".format(malformed)
-        print "\n"
+MAGIC_CHAR = u'ď'
+words_offset = {}
+translated_words = {}
 
 
 def main():
+    print "Opening the dictionary file..."
+    with io.open("dictionary.txt", "r", encoding="utf-8") as file:
+        scrabble_file = file.read()
     print "Opening the file..."
-    with io.open("dictionary.txt", "rt", encoding="utf-8") as scrabble_file:
+    with io.open("test.txt", "r", encoding="utf-8") as file:
+        data_file = file.read()
 
+    data_file = re.sub(
+        re.compile(u'ż˝', re.UNICODE),
+        '',
+        data_file
+    )
 
-        print "Parsing words..."
-        for word in scrabble_file:
-            word = word.replace(u'\n', u'')
+    with io.open("test.txt", "w", encoding="utf-8") as file:
+        file.write(data_file)
 
-            word = word.replace(u',', u'')
-            encoded_word = transform(word.lower())
-            dictionary.setdefault((encoded_word), []).append(word)
-        print "Finished"
-        # pojedyncze slowo
-        question()
+    while True:
+        m = re.search(
+            re.compile(
+                u'\b.+{}.+\b'.format(MAGIC_CHAR),
+                re.UNICODE
+            ),
+            data_file,
+        )
+        if m is None:
+            break
+        words_offset.setdefaults(m.group, default=[]).append(m.start)
 
-        # TODO: zdania
-        # enter_text()
-                # encoded_word = u"".join(mapping[c] for c in word)
+    print "Finished!"
 
+    print words_offset
 
+    print "Parsing words..."
+
+    for word in words_offset.iterkeys():
+        temp = word
+        temp.replace(MAGIC_CHAR, u'{}')
+        m = re.match(temp.format('|'.join(maps)), scrabble_file)
+        translated_words.setdefault(word, default=[]).append(m.groups)
+    print "Finished!"
+
+    print "Opening the file..."
+
+    print "Changing know words..."
+
+    for word in words_offset.iterkeys():
+        if translated_words[word].len() == 1:
+            for offset in words_offset[word]:
+                data_file.seek(offset)
+                data_file.write(translated_words[word][0])
+        else:
+            print "Cannot change word - '{}'".format(word)
+
+    with io.open("test.txt", "w", encoding="utf-8") as file:
+        file.write(data_file)
+
+    print "Finished!"
 
 if __name__ == "__main__":
     main()
